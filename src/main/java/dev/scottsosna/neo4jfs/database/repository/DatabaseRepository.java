a27bc2bc79aff81a1b72990a9c1a88a7eca29503
Scott Sosna <me@scottsosna.dev>
2025-12-03 21:24:29 -0600

    Code documentation.

--- src/main/java/dev/scottsosna/neo4jfs/database/repository/DatabaseRepository.java
+++ src/main/java/dev/scottsosna/neo4jfs/database/repository/DatabaseRepository.java
@@ -7,9 +7,41 @@ import java.util.List;
 
 public interface DatabaseRepository {
 
-    Database create(URI uri);
-    void drop(URI uri);
-    Database find(URI uri);
+    /**
+     * Create new database based on URI
+     *
+     * @param fsUri Neo4Jfs file system URI for the database to be created
+     * @return database just created
+     */
+    Database create(URI fsUri);
+
+    /**
+     * Drop an existing database from the Neo4J instance.
+     *
+     * @param fsUri Neo4J file system URI for the database to be dropped
+     */
+    void drop(URI fsUri);
+
+    /**
+     * Return a single database by, based on the "host" in the URI
+     *
+     * @param fsUri Neo4Jfs URI for database to be retrieved.
+     * @return database found or null
+     */
+    Database find(URI fsUri);
+
+    /**
+     * Return a single database by name
+     *
+     * @param dbName name of requested database
+     * @return database found or null
+     */
     Database find(String dbName);
+
+    /**
+     * Return existing Neo4J databases
+     *
+     * @return list of daatabases currently in Neo4J instance.
+     */
     List<Database> findAll();
 }
--- src/main/java/dev/scottsosna/neo4jfs/database/repository/DatabaseRepositoryImpl.java
+++ src/main/java/dev/scottsosna/neo4jfs/database/repository/DatabaseRepositoryImpl.java
@@ -10,68 +10,90 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.StreamSupport;
 
+/**
+ * Functionality for managing Neo4J databases.  Note that"databases are not graph objects, so
+ * commands not Cypher queries are used.
+ */
 @Component
 public class DatabaseRepositoryImpl extends BaseEntryRepositoryImpl implements DatabaseRepository {
 
+    /**
+     * Neo4J commands for working with databases.
+     */
+    private static final String CREATE_DATABASE = "CREATE DATABASE $database";
+    private static final String DROP_DATABASE = "DROP DATABASE $database";
+    private static final String SHOW_DATABASE = "SHOW DATABASE $database";
+    private static final String SHOW_DATABASES = "SHOW DATABASES";
+    private static final String PARAMETER_DATABASE = "database";
+
+    /**
+     * Constructor
+     * @param config configuration bean hold Neo4J connection and authentication credentials.
+     */
     public DatabaseRepositoryImpl(Neo4jfsConfiguration config) {
         super(config);
     }
 
     /**
      * Create new database based on URI
-     * @param uri base URI of the file system
+     *
+     * @param fsUri Neo4Jfs file system URI for the database to be created
      * @return database just created
      */
     @Override
-    public Database create(URI uri) {
-        String dbName = uri.getHost();
-        query("CREATE DATABASE $database", Map.of("database", dbName));
+    public Database create(URI fsUri) {
+        String dbName = fsUri.getHost();
+        query(CREATE_DATABASE, Map.of(PARAMETER_DATABASE, dbName));
         return find(dbName);
     }
 
     /**
      * Drop an existing database from the Neo4J instance.
-     * @param uri base URI of the file system
+     *
+     * @param fsUri Neo4J file system URI for the database to be dropped
      */
-    public void drop(URI uri) {
-        query("DROP DATABASE $database", Map.of("database", uri.getHost()));
+    public void drop(URI fsUri) {
+        query(DROP_DATABASE, Map.of(PARAMETER_DATABASE, fsUri.getHost()));
     }
 
     /**
      * Return a single database by, based on the "host" in the URI
-     * @param uri URI for the Neo4J filesystem
+     *
+     * @param fsUri Neo4Jfs URI for database to be retrieved.
      * @return database found or null
      */
     @Override
-    public Database find(URI uri) {
-        return find(uri.getHost());
+    public Database find(URI fsUri) {
+        return find(fsUri.getHost());
     }
 
     /**
      * Return a single database by name
+     *
      * @param dbName name of requested database
      * @return database found or null
      */
     @Override
     public Database find(String dbName) {
-        Result r = query("SHOW DATABASE $database", Map.of("database", dbName));
+        Result r = query(SHOW_DATABASE, Map.of(PARAMETER_DATABASE, dbName));
         List<Database> dbs = deserialize(r);
         return (dbs.isEmpty() ? null : dbs.getFirst());
     }
 
     /**
-     * Return all databases
+     * Return existing Neo4J databases
+     *
      * @return list of daatabases currently in Neo4J instance.
      */
     @Override
     public List<Database> findAll() {
-        Result r = query("SHOW DATABASES");
+        Result r = query(SHOW_DATABASES);
         return deserialize(r);
     }
 
-
     /**
      * Take results of query and deserialize into Database objects.
+     *
      * @param r Query results
      * @return list of zero or more Database objects
      */
