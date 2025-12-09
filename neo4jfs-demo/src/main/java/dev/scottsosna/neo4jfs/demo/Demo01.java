4d90a66364d5d85d7d4a452bb91c03b307140a9a
Scott Sosna <me@scottsosna.dev>
2025-12-08 18:50:08 -0600

    First working demo program.

--- neo4jfs-demo/src/main/java/dev/scottsosna/neo4jfs/demo/Demo01.java
+++ neo4jfs-demo/src/main/java/dev/scottsosna/neo4jfs/demo/Demo01.java
@@ -1,21 +1,54 @@
 package dev.scottsosna.neo4jfs.demo;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 import java.net.URI;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Map;
 
-@ComponentScan("dev.scottsosna.neo4jfs")
-@SpringBootApplication
-public class Demo01 {
+/**
+ * Demo01: Create a new Neo4Jfs file system, create directories, load files.  Nothing too fancy.
+ */
+@SpringBootApplication(scanBasePackages = {"dev.scottsosna.neo4jfs", "dev.scottsosna.neo4jfs.demo"})
+public class Demo01 implements CommandLineRunner {
 
     public static void main(String[] args) {
-        try (FileSystem fs = FileSystems.newFileSystem(URI.create("neo4j://mydemo"), Map.of())) {
-            Files.createDirectory(fs.getPath("/abc"));
+        SpringApplication.run(Demo01.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        try (FileSystem fs = FileSystems.newFileSystem(URI.create("neo4jfs://neo4jfs-demo"), Map.of())) {
+            //  Create directories for different media types.
+            Files.createDirectory(fs.getPath("/songs"));
+            Files.createDirectory(fs.getPath("/videos"));
+            Files.createDirectory(fs.getPath("/images"));
+            Files.createDirectory(fs.getPath("/copies"));
+
+            //  Upload images: note that cross-filesystem copies check for existence of target unless
+            //  {@code StandardCopyOption.REPLACE_EXISTING} is specified.  However, the existence check does not
+            //  know whether the target is a file or directory, therefore must explicitly state target name.
+            Files.copy(Path.of("data/IMG_0308.jpg"), fs.getPath("/images/IMG_0308.jpg"));
+            Files.copy(Path.of("data/IMG_0324.jpg"), fs.getPath("/images/IMG_0324.jpg"));
+            Files.copy(Path.of("data/IMG_0398.jpg"), fs.getPath("/images/IMG_0398.jpg"));
+
+            //  Make copies of original images in the "copies" directory"  Within a file system, the move
+            //  can happen into a directory correctly.
+            Files.copy(fs.getPath("images/IMG_0308.jpg"), fs.getPath("copies"));
+            Files.copy(fs.getPath("images/IMG_0324.jpg"), fs.getPath("copies"));
+            Files.copy(fs.getPath("images/IMG_0398.jpg"), fs.getPath("copies"));
+
+            //  Move the original directories into a "data" directory.
+            Files.createDirectory(fs.getPath("/data"));
+            Files.move(fs.getPath("/songs"), fs.getPath("/data"));
+            Files.move(fs.getPath("/videos"), fs.getPath("/data"));
+            Files.move(fs.getPath("/images"), fs.getPath("/data"));
+            Files.move(fs.getPath("/copies"), fs.getPath("/data"));
         } catch (Exception e) {
             e.printStackTrace();
         }
