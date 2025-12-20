dc28468624f85b67f4e00be4f142077a5a3ffaa6
scsosna99 <me@scottsosna.dev>
2025-12-19 19:59:46 -0600

    Dummy storage manager that doesn't actually store files, useful for development purposes when working with file/directory tree and storing actual files is not necessary or is not feasible (such as a large file/directory tree)

---
+++ neo4jfs/src/main/java/dev/scottsosna/neo4jfs/storage/DummyStorageManager.java
@@ -0,0 +1,95 @@
+package dev.scottsosna.neo4jfs.storage;
+
+import dev.scottsosna.neo4jfs.database.model.storage.StorageFileInfo;
+import dev.scottsosna.neo4jfs.storage.util.LocalStorageFileStore;
+import dev.scottsosna.neo4jfs.storage.util.NullSeekableByteChannel;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.file.FileStore;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.util.Random;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * Dummy storage manager that emulates files stored externally without actually doing so.  Intended for development
+ * when working on file tree functionality and actual files are unnecessary or bothersome.
+ */
+@Service("dummy")
+@ConditionalOnProperty(prefix = "neo4jfs", name = "storage", havingValue = "dummy")
+public class DummyStorageManager implements StorageManager {
+
+    Random random = new Random();
+
+    public void initPartition(final URI fsUri) throws IOException {
+    }
+
+    public void dropPartition(final URI fsUri) throws IOException {
+    }
+
+    public FileStore getPartitionFileStore(final URI fsUri) throws IOException {
+        String partitionName = fsUri.getHost();
+        return new LocalStorageFileStore(Path.of("/dev/null", partitionName));
+    }
+
+    @Override
+    public StorageFileInfo createFile(final URI uri) throws IOException {
+        return new StorageFileInfo(generateRelativePath().toString(), 0);
+    }
+
+    @Override
+    public StorageFileInfo createFile(final URI uri, final InputStream is) throws IOException {
+        return new StorageFileInfo(generateRelativePath().toString(), random.nextInt(1024, 40960));
+    }
+    @Override
+    public StorageFileInfo updateFile(final URI uri,
+                                      final String storageId,
+                                      final InputStream is) throws IOException {
+
+        return new StorageFileInfo(generateRelativePath().toString(), random.nextInt(1024, 40960));
+    }
+
+    public StorageFileInfo copyFile(final URI fsUri, final String storageId) throws IOException{
+        return getFileInfo(fsUri, generateRelativePath().toString());
+    }
+
+    @Override
+    public StorageFileInfo getFileInfo(final URI fsUri, final String storageId) throws IOException {
+        return new StorageFileInfo(storageId, random.nextInt(1024, 40960));
+    }
+
+    @Override
+    public OutputStream getFileOutputStream(final URI uri, final String storageId) throws IOException {
+        return OutputStream.nullOutputStream();
+    }
+
+    public SeekableByteChannel getSeekableByteChannel(final URI fsUri,
+                                                      final String storageId,
+                                                      final Set<? extends OpenOption> options) throws IOException {
+        return new NullSeekableByteChannel();
+    }
+
+    @Override
+    public InputStream getFileInputStream(final URI uri, final String storageId) throws IOException {
+        byte[] bytes = new byte[random.nextInt(1024, 40960)];
+        random.nextBytes(bytes);
+        return new ByteArrayInputStream(bytes);
+    }
+
+    @Override
+    public void deleteFile(final URI fsUri, final String storageId) throws IOException {
+    }
+
+    private Path generateRelativePath() {
+        String uuid = UUID.randomUUID().toString();
+        return Path.of(uuid.substring(0, 2), uuid);
+    }
+}
---
+++ neo4jfs/src/main/java/dev/scottsosna/neo4jfs/storage/util/NullSeekableByteChannel.java
@@ -0,0 +1,63 @@
+package dev.scottsosna.neo4jfs.storage.util;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SeekableByteChannel;
+import java.util.Random;
+
+/**
+ * Do-nothing SeekableByteChannel that acts like there's a backing file but in fact does nothing usage with physical
+ * files.  Used in conjunction with DummyStorageManager as a development aid.
+ */
+public class NullSeekableByteChannel implements SeekableByteChannel {
+
+    private boolean closed = false;
+    private long position = 0;
+    private Random random = new Random();
+
+    @Override
+    public int read(ByteBuffer dst) throws IOException {
+        int size = dst.remaining();
+        byte[] bytes = new byte[size];
+        random.nextBytes(bytes);
+        dst.put(bytes);
+        position += size;
+        return size;
+    }
+
+    @Override
+    public int write(ByteBuffer src) throws IOException {
+        position += src.remaining();
+        return src.remaining();
+    }
+
+    @Override
+    public long position() throws IOException {
+        return position;
+    }
+
+    @Override
+    public SeekableByteChannel position(long newPosition) throws IOException {
+        return this;
+    }
+
+    @Override
+    public long size() throws IOException {
+        return 0;
+    }
+
+    @Override
+    public SeekableByteChannel truncate(long size) throws IOException {
+        return null;
+    }
+
+    @Override
+    public boolean isOpen() {
+        return !closed;
+    }
+
+    @Override
+    public void close() throws IOException {
+        closed = true;
+    }
+}
