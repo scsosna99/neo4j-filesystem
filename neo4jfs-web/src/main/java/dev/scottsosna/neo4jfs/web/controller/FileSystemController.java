ff4fb878366d2a2990e2ae7f876aa16a5c4a5f31
Scott Sosna <me@scottsosna.dev>
2025-12-27 20:24:01 -0600

    Added copyright and licensing information to all source files.

--- neo4jfs-web/src/main/java/dev/scottsosna/neo4jfs/web/controller/FileSystemController.java
+++ neo4jfs-web/src/main/java/dev/scottsosna/neo4jfs/web/controller/FileSystemController.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2025 Scott C. Sosna.  All rights reserved.
+ *
+ * Licensed under the MIT license for non-commercial use.  Please refer to LICENSE-MIT.md or
+ * https://opensource.org/license/mit for terms and conditions.
+ *
+ * Licensed under the GPLv3 license for commercial use.  Please refer to LICENSE-GPL.md or
+ * https://www.gnu.org/licenses/gpl-3.0.html for terms and conditions.
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * expressed or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package dev.scottsosna.neo4jfs.web.controller;
 
 import dev.scottsosna.neo4jfs.service.FileSystemService;
