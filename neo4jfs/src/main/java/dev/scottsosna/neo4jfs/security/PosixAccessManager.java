6c92225fc3f01f2213d872636ffb4a7b95d5ed84
Scott Sosna <me@scottsosna.dev>
2026-01-12 17:40:37 -0600

    need to use inherited permissions when entry has no specific permissions.

--- neo4jfs/src/main/java/dev/scottsosna/neo4jfs/security/PosixAccessManager.java
+++ neo4jfs/src/main/java/dev/scottsosna/neo4jfs/security/PosixAccessManager.java
@@ -99,7 +99,11 @@ public class PosixAccessManager implements AccessManager {
      */
     public AccessMode[] checkAccess(BaseEntry entry, AccessMode... modes) {
         if (modes == null || modes.length == 0) return modes;
-        return checkAccessWork(entry.getOwnerUserName(), entry.getOwnerGroupName(), entry.getPermissions(), modes);
+        return checkAccessWork(
+            entry.getOwnerUserName(),
+            entry.getOwnerGroupName(),
+            entry.getPermissions() != null ? entry.getPermissions() : entry.getInheritedPermissions(),
+            modes);
     }
 
     /**
