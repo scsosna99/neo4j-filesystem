4ebe371b72f5dde1d9d5708c42752a4c50f758e0
Scott Sosna <me@scottsosna.dev>
2026-02-09 17:13:31 -0600

    Optimize imports

--- neo4jfs-web/src/main/java/dev/scottsosna/neo4jfs/web/controller/FileSystemController.java
+++ neo4jfs-web/src/main/java/dev/scottsosna/neo4jfs/web/controller/FileSystemController.java
@@ -15,10 +15,10 @@
 package dev.scottsosna.neo4jfs.web.controller;
 
 import dev.scottsosna.neo4jfs.service.FileSystemService;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
